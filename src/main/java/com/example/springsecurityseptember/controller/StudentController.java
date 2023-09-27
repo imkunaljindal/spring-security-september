1c4e823207e6a6dcdcd1d14d5c7482c51cf06506
Kunal Jindal <kunaljindal@mnnit.ac.in>
2023-09-27 23:13:23 +0530

    Add Role based authentication using in memory

---
+++ src/main/java/com/example/springsecurityseptember/controller/StudentController.java
@@ -0,0 +1,20 @@
+package com.example.springsecurityseptember.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to STUDENT area!!!";
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello STUDENT";
+    }
+}
